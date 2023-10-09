d7ca2d4f91efc8f926f585262f60404ce3cabfb3
Kim_Chae_Young <yong08291@gamil.com>
2023-10-09 12:32:50 +0900

    음악 추가6(구매는 아직..)

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -47,57 +47,6 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
-//        for(int i =1; i <= 3; i++) {
-//            ImageButton imagePlayButton = findViewById(getResources().getIdentifier("imagePlayButton" + i,"id",getPackageName()));
-//            ImageButton imageStopButton = findViewById(getResources().getIdentifier("imageStopButton" + i, "id", getPackageName()));
-//            MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier("my_song" + i, "raw", getPackageName()));
-//
-//            imagePlayButton.setTag(i);
-//            imagePlayButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    int buttonIndex = (int) view.getTag();
-//                    playMusic(buttonIndex);
-//                }
-//
-//                private void playMusic(int buttonIndex) {
-//                    MediaPlayer mediaPlayer = mediaPlayers.get(buttonIndex -1);
-//                    mediaPlayer.start();
-//
-//                    ImageButton imagePlayButton = imagePlayButtons.get(buttonIndex-1);
-//                    ImageButton imageStopButton = imageStopButtons.get(buttonIndex-1);
-//
-//                    imagePlayButton.setVisibility(View.GONE);
-//                    imageStopButton.setVisibility(View.VISIBLE);
-//
-//                }
-//            });
-//
-//            imageStopButton.setTag(i);
-//            imageStopButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    int buttonIndex = (int) view.getTag();
-//                    pauseMusic(buttonIndex);
-//                }
-//
-//                private void pauseMusic(int buttonIndex) {
-//                    MediaPlayer mediaPlayer = mediaPlayers.get(buttonIndex - 1);
-//                    mediaPlayer.pause();
-//
-//                    ImageButton imagePlayButton = imagePlayButtons.get(buttonIndex - 1);
-//                    ImageButton imageStopButton = imageStopButtons.get(buttonIndex - 1);
-//
-//                    imagePlayButton.setVisibility(View.VISIBLE);
-//                    imageStopButton.setVisibility(View.GONE);
-//                }
-//            });
-//
-//            imagePlayButtons.add(imagePlayButton);
-//            imageStopButtons.add(imageStopButton);
-//            mediaPlayers.add(mediaPlayer);
-//        }
-//    }
         imagePlayButton1 = findViewById(R.id.imagePlayButton1);
         imageStopButton1 =findViewById(R.id.imageStopButton1);
         imagePlayButton2 = findViewById(R.id.imagePlayButton2);
@@ -112,6 +61,12 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 playMusic1();
             }
+
+            public void playMusic1() {
+                mediaPlayer1.start();
+                imagePlayButton1.setVisibility(View.GONE);
+                imageStopButton1.setVisibility(View.VISIBLE);
+            }
         });
 
         imageStopButton1.setOnClickListener(new View.OnClickListener() {
@@ -119,6 +74,13 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 pauseMusic1();
             }
+            public void pauseMusic1(){
+                mediaPlayer1.pause();
+                imagePlayButton1.setVisibility(View.VISIBLE);
+                imageStopButton1.setVisibility(View.GONE);
+
+            }
+
         });
         imagePlayButton2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,16 +108,5 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
     }
-    public void playMusic1() {
-        mediaPlayer1.start();
-        imagePlayButton1.setVisibility(View.GONE);
-        imageStopButton1.setVisibility(View.VISIBLE);
-    }
 
-    public void pauseMusic1(){
-        mediaPlayer1.pause();
-        imagePlayButton1.setVisibility(View.VISIBLE);
-        imageStopButton1.setVisibility(View.GONE);
-
-    }
 }

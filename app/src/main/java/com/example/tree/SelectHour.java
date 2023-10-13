9ca67b6611fa180963e9bc6409679618008dae53
suh0 <suho__kim@naver.com>
2023-10-14 05:34:06 +0900

    add TestCase, modify TimeTable, improve Algo

--- app/src/main/java/com/example/tree/SelectHour.java
+++ app/src/main/java/com/example/tree/SelectHour.java
@@ -9,9 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SelectHour extends AppCompatActivity {
 
+    Button show5s;
+    Button show30m;
     Button show1h;
     Button show2h;
-    Button show3h;
     ImageView treeImage;
     TextView treeInfo;
     Button prev;
@@ -19,16 +20,14 @@ public class SelectHour extends AppCompatActivity {
     Button confirm;
 
     //index는 1부터
-    int currentHour = 1;
+    int currentHour_number = 1;
     int currentTreeIndex = 1;
-    final int max_1h = 3;
-    final int max_2h = 3;
-    final int max_3h = 3;
+    final int max_tree_index = 3;
 
     // 화면 업데이트
     void updateTree()
     {
-        switch(currentHour)
+        switch(currentHour_number)
         {
             case 1:
                 if(currentTreeIndex == 1)
@@ -83,6 +82,24 @@ public class SelectHour extends AppCompatActivity {
                     treeInfo.setText("나무 3-3");
                 }
                 break;
+
+            case 4:
+                if(currentTreeIndex == 1)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_1);
+                    treeInfo.setText("나무 4-1");
+                }
+                else if(currentTreeIndex == 2)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 4-2");
+                }
+                else if(currentTreeIndex == 3)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 4-3");
+                }
+                break;
         }
     }
 
@@ -92,9 +109,10 @@ public class SelectHour extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_hour);
 
+        show5s = findViewById(R.id.show5s);
+        show30m = findViewById(R.id.show30m);
         show1h = findViewById(R.id.show1h);
         show2h = findViewById(R.id.show2h);
-        show3h = findViewById(R.id.show3h);
         treeImage = (ImageView) findViewById(R.id.treeImage);
         treeInfo = findViewById(R.id.treeInfo);
         prev = findViewById(R.id.prev);
@@ -103,17 +121,21 @@ public class SelectHour extends AppCompatActivity {
         updateTree();
 
         // 시간 선택
+        show5s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { currentHour_number = 1; updateTree(); }
+        });
+        show30m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { currentHour_number = 2; updateTree(); }
+        });
         show1h.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { currentHour = 1; updateTree(); }
+            public void onClick(View view) { currentHour_number = 3; updateTree(); }
         });
         show2h.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { currentHour = 2; updateTree(); }
-        });
-        show3h.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { currentHour = 3; updateTree(); }
+            public void onClick(View view) { currentHour_number = 4; updateTree(); }
         });
 
         // '이전' 버튼
@@ -122,14 +144,7 @@ public class SelectHour extends AppCompatActivity {
             public void onClick(View view)
             {
                 if(currentTreeIndex <= 1)
-                {
-                    if(currentHour == 1)
-                        currentTreeIndex = max_1h;
-                    else if(currentHour == 2)
-                        currentTreeIndex = max_2h;
-                    else if(currentHour == 3)
-                        currentTreeIndex = max_3h;
-                }
+                    currentTreeIndex = max_tree_index;
                 else
                     currentTreeIndex--;
 
@@ -141,11 +156,7 @@ public class SelectHour extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                if(currentHour == 1 && currentTreeIndex >= max_1h)
-                    currentTreeIndex = 1;
-                else if(currentHour == 2 && currentTreeIndex >= max_2h)
-                    currentTreeIndex = 1;
-                else if(currentHour == 3 && currentTreeIndex >= max_3h)
+                if(currentTreeIndex >= max_tree_index)
                     currentTreeIndex = 1;
                 else
                     currentTreeIndex++;

77db83cd721856bedda662f475d550824646c25d
Chaeyoung <yong08291@gmail.com>
2023-11-12 21:56:38 +0900

    타이머

---
+++ app/src/main/java/com/example/tree/SelectHour.java
@@ -0,0 +1,218 @@
+package com.example.tree;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SelectHour extends AppCompatActivity {
+
+    Button show5s;
+    Button show30m;
+    Button show1h;
+    Button show2h;
+    ImageView treeImage;
+    TextView treeInfo;
+    Button prev;
+    Button next;
+    Button confirm;
+
+    //index는 1부터
+    int currentHour_number = 1;
+    int currentTreeIndex = 1;
+    final int max_tree_index = 3;
+
+    // 화면 업데이트
+    void updateTree()
+    {
+        switch(currentHour_number)
+        {
+            case 1:
+                if(currentTreeIndex == 1)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_1);
+                    treeInfo.setText("나무 1-1");
+                }
+                else if(currentTreeIndex == 2)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 1-2");
+                }
+                else if(currentTreeIndex == 3)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 1-3");
+                }
+                break;
+
+            case 2:
+                if(currentTreeIndex == 1)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_1);
+                    treeInfo.setText("나무 2-1");
+                }
+                else if(currentTreeIndex == 2)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 2-2");
+                }
+                else if(currentTreeIndex == 3)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 2-3");
+                }
+                break;
+
+            case 3:
+                if(currentTreeIndex == 1)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_1);
+                    treeInfo.setText("나무 3-1");
+                }
+                else if(currentTreeIndex == 2)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 3-2");
+                }
+                else if(currentTreeIndex == 3)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 3-3");
+                }
+                break;
+
+            case 4:
+                if(currentTreeIndex == 1)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_1);
+                    treeInfo.setText("나무 4-1");
+                }
+                else if(currentTreeIndex == 2)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 4-2");
+                }
+                else if(currentTreeIndex == 3)
+                {
+                    treeImage.setImageResource(R.drawable.tree_1_2);
+                    treeInfo.setText("나무 4-3");
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_hour);
+
+        show5s = findViewById(R.id.show5s);
+        show30m = findViewById(R.id.show30m);
+        show1h = findViewById(R.id.show1h);
+        show2h = findViewById(R.id.show2h);
+        treeImage = (ImageView) findViewById(R.id.treeImage);
+        treeInfo = findViewById(R.id.treeInfo);
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
+        confirm = findViewById(R.id.confirm);
+        updateTree();
+
+        // 시간 선택
+        show5s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { currentHour_number = 1; updateTree(); }
+        });
+        show30m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { currentHour_number = 2; updateTree(); }
+        });
+        show1h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { currentHour_number = 3; updateTree(); }
+        });
+        show2h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { currentHour_number = 4; updateTree(); }
+        });
+
+        // '이전' 버튼
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(currentTreeIndex <= 1)
+                    currentTreeIndex = max_tree_index;
+                else
+                    currentTreeIndex--;
+
+                updateTree();
+            }
+        });
+        // '다음' 버튼
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(currentTreeIndex >= max_tree_index)
+                    currentTreeIndex = 1;
+                else
+                    currentTreeIndex++;
+
+                updateTree();
+            }
+        });
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long selected_milliseconds = 0;
+                switch (currentHour_number){
+                    case 1:
+                        selected_milliseconds = 5000; //1000(1초) * 5
+                        break;
+                    case 2:
+                        selected_milliseconds = 1000 * 60 * 30; //30분 1000(1초) * 60(1분) * 30
+                        break;
+                    case 3:
+                        selected_milliseconds = 1000 * 60 * 60; //1시간
+                        break;
+                    case 4:
+                        selected_milliseconds = 1000 * 60 * 60 * 2; //2시간
+                        break;
+                }
+
+                Intent intent = new Intent(SelectHour.this, TimerActivity.class);
+                intent.putExtra("selected_milliseconds", selected_milliseconds); // 변경된 부분
+
+                startActivityForResult(intent, 1); //넘기기
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("result");
+                if (result.equals("실패하였습니다!")) {
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show(); // 실패 메시지를 토스트로 출력
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tree/TimerActivity.java
@@ -0,0 +1,147 @@
+package com.example.tree;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class TimerActivity extends AppCompatActivity {
+
+    private TextView timerTextView;
+    private CountDownTimer countDownTimer;
+    private ImageButton exitButton;
+    private RecordDatabaseHelper dbHelper;
+    private long selectedMilliseconds;
+    private ProgressBar progressBarCircle;
+
+    private ImageView timer_image1;
+    private ImageView timer_image2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        exitButton = findViewById(R.id.exitButton);
+        progressBarCircle = findViewById(R.id.progressBarCircle);
+        timer_image1 = findViewById(R.id.timer_image1);
+        timer_image2 = findViewById(R.id.timer_image2);
+
+        dbHelper = new RecordDatabaseHelper(this);
+
+        selectedMilliseconds = getIntent().getLongExtra("selected_milliseconds", 1000);
+
+
+
+        countDownTimer = new CountDownTimer(selectedMilliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //long seconds = millisUntilFinished / 1000;
+                int seconds = (int) (millisUntilFinished / 1000);
+                int totalSeconds = (int) (selectedMilliseconds / 1000);
+                int remainingSeconds = (int) (millisUntilFinished / 1000);
+                updateTimer(seconds);
+                progressBarCircle.setProgress(seconds);
+                setupProgressBar();
+                updateTimer(remainingSeconds);
+                progressBarCircle.setProgress(remainingSeconds);
+
+                if (remainingSeconds <= totalSeconds / 2) {
+                    // 타이머 시간이 절반이 지나면 timer_image1을 INVISIBLE로 설정
+                    timer_image1.setVisibility(View.INVISIBLE);
+
+                    // timer_image2를 VISIBLE로 설정
+                    timer_image2.setVisibility(View.VISIBLE);
+                }
+                //String time = String.format("%02d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, seconds % 60);
+                //timerTextView.setText(time);
+            }
+
+            @Override
+            public void onFinish() {
+                timerTextView.setText("성공!");
+                saveRecord();
+                returnToMainScreen();
+            }
+        };
+
+        countDownTimer.start();
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+                finishWithFailure();
+            }
+        });
+
+    }
+
+    private void setupProgressBar() {  //이거는 꽉 채운 상태부터 감소하는 것
+        progressBarCircle = findViewById(R.id.progressBarCircle);
+        //"timeCountInMilliSeconds"는 원형 프로그래스 바의 최대값을 설정하는 데 사용
+
+        int maxProgress=(int) (selectedMilliseconds / 1000);
+
+        progressBarCircle.setMax(maxProgress);
+    }
+
+    private void updateTimer(int seconds) {
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+        String time = String.format("%02d:%02d:%02d", hours, minutes, secs);
+        timerTextView.setText(time);
+        progressBarCircle.setProgress(seconds);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        countDownTimer.cancel(); // 액티비티 종료 시 타이머 초기화
+        dbHelper.close();
+    }
+
+    private void saveRecord() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("date", getCurrentDate());
+        values.put("duration", selectedMilliseconds);
+        db.insert("records", null, values);
+        db.close();
+    }
+
+    private String getCurrentDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    private void finishWithFailure() {
+        Intent intent = new Intent();
+        intent.putExtra("result", "실패하였습니다!");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    private void returnToMainScreen() {
+        Intent intent = new Intent(TimerActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}

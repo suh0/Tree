103d836c17a098afc68de0803faa27f465bf66fd
Chaeyoung <yong08291@gmail.com>
2023-11-14 01:05:31 +0900

    타이머 배경음악

--- app/src/main/java/com/example/tree/TimerActivity.java
+++ app/src/main/java/com/example/tree/TimerActivity.java
@@ -2,12 +2,15 @@ package com.example.tree;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-
+import android.net.Uri;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -17,10 +20,29 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class TimerActivity extends AppCompatActivity {
 
     private TextView timerTextView;
@@ -33,6 +55,17 @@ public class TimerActivity extends AppCompatActivity {
 
     private ImageView timer_image1;
     private ImageView timer_image2;
+    private ImageButton backmusic_start;
+    private ImageButton backmusic_stop;
+
+    private static final String TAG_TEXT ="music";
+    private boolean isMusicPlaying = false;
+
+
+
+    List<Map<String, Object>> dialogItemList;
+    String[] musicFiles = {"music03.mp3", "music04.mp3", "music05.mp3"};
+    private MediaPlayer mediaPlayer;
 
 
     @Override
@@ -45,12 +78,13 @@ public class TimerActivity extends AppCompatActivity {
         progressBarCircle = findViewById(R.id.progressBarCircle);
         timer_image1 = findViewById(R.id.timer_image1);
         timer_image2 = findViewById(R.id.timer_image2);
+        backmusic_start = findViewById(R.id.backmusic_start);
+        backmusic_stop = findViewById(R.id.backmusic_stop);
 
 
         dbHelper = new RecordDatabaseHelper(this);
 
         selectedMilliseconds = getIntent().getLongExtra("selected_milliseconds", 1000);
-
 
 
         countDownTimer = new CountDownTimer(selectedMilliseconds, 1000) {
@@ -75,34 +109,136 @@ public class TimerActivity extends AppCompatActivity {
                     timer_image2.setVisibility(View.VISIBLE);
                 }
             }
-                //String time = String.format("%02d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, seconds % 60);
-                //timerTextView.setText(time);
+            //String time = String.format("%02d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, seconds % 60);
+            //timerTextView.setText(time);
+
+
+            @Override
+            public void onFinish() {
+                timerTextView.setText("성공!");
+                saveRecord();
+                returnToMainScreen();
+            }
+        };
+
+        countDownTimer.start();
 
 
 
+        backmusic_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
+        dialogItemList = new ArrayList<>();
 
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+                finishWithFailure();
+            }
+        });
+        backmusic_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(TimerActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_dialog, null);
+        builder.setView(view);
+
+        final ListView listview = (ListView) view.findViewById(R.id.listview_alterdialog_list);
+        final AlertDialog dialog = builder.create();
+
+        // dialogItemList 초기화 및 데이터 추가
+        dialogItemList = new ArrayList<>();
+        for (String music : musicFiles) {
+            Map<String, Object> item = new HashMap<>();
+            item.put(TAG_TEXT, music);
+            dialogItemList.add(item);
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(TimerActivity.this, dialogItemList,
+                R.layout.dialog_row,
+                new String[]{TAG_TEXT},
+                new int[]{R.id.alertDialogItemTextView});
+        listview.setAdapter(simpleAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedMusic = musicFiles[position]; // 선택된 음악 파일 이름 가져오기
+                playMusic(selectedMusic); // 음악 재생 코드 추가
+                dialog.dismiss(); // 다이얼로그 닫기
+                backmusic_start.setVisibility(View.GONE);
+                backmusic_stop.setVisibility(View.VISIBLE);
+            }
+        });
+        dialog.setCancelable(false);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    private void playMusic(String musicFileName) {
+        if (mediaPlayer != null) {  // MediaPlayer 사용 전에 먼저 release() 호출
+            mediaPlayer.reset();
+        } else {
+            mediaPlayer = new MediaPlayer();
+        }
+        int resId = 0; // 여기에 리소스 ID를 직접 지정
+
+        // 음악 파일 이름에 따라 리소스 ID 설정
+        if (musicFileName.equals("music03.mp3")) {
+            resId = R.raw.music03;
+        } else if (musicFileName.equals("music04.mp3")) {
+            resId = R.raw.music04;
+        } else if (musicFileName.equals("music05.mp3")) {
+            resId = R.raw.music05;
+        }
+       // MediaPlayer mediaPlayer = new MediaPlayer();
+        //mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+          //      .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            //    .build());
+
+        try {
+            AssetFileDescriptor afd = getResources().openRawResourceFd(resId);
+            if (afd != null) {
+                mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                afd.close();
+
+                mediaPlayer.prepareAsync();
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                     @Override
-                    public void onFinish() {
-                        timerTextView.setText("성공!");
-                        saveRecord();
-                        returnToMainScreen();
-                    }
-                };
-
-                countDownTimer.start();
-
-                exitButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        countDownTimer.cancel();
-                        finishWithFailure();
+                    public void onPrepared(MediaPlayer mp) {
+                        mediaPlayer.start();
                     }
                 });
-
-
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        backmusic_start.setVisibility(View.GONE);
+        backmusic_stop.setVisibility(View.VISIBLE);
+        isMusicPlaying = true;
+    }
 
-            private void setupProgressBar() {  //이거는 꽉 채운 상태부터 감소하는 것
+    private void stopMusic() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        backmusic_start.setVisibility(View.VISIBLE);
+        backmusic_stop.setVisibility(View.GONE);
+    }
+
+    private void setupProgressBar() {  //이거는 꽉 채운 상태부터 감소하는 것
                 progressBarCircle = findViewById(R.id.progressBarCircle);
                 //"timeCountInMilliSeconds"는 원형 프로그래스 바의 최대값을 설정하는 데 사용
 
@@ -122,6 +258,10 @@ public class TimerActivity extends AppCompatActivity {
 
             @Override
             protected void onDestroy() {
+                if (mediaPlayer != null) {
+                    mediaPlayer.reset();
+                    mediaPlayer.release();
+                }
                 super.onDestroy();
                 countDownTimer.cancel(); // 액티비티 종료 시 타이머 초기화
                 dbHelper.close();

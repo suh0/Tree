f7a70fbef3407ac76c960bee9f3604fd21b4ebfe
eSeohyeon <leeseohyeon1203@gmail.com>
2023-11-28 16:02:25 +0900

    배경, 버튼 수정

--- app/src/main/java/com/example/tree/MainActivity.java
+++ app/src/main/java/com/example/tree/MainActivity.java
@@ -5,27 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button plant_btn;
-    Button record_btn; // "기록" 버튼 추가
-
-    Button shop_btn;
+    ImageView btn_timer, btn_user, btn_shop;
+    Button record_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        plant_btn = findViewById(R.id.plant_btn);
+        btn_timer = findViewById(R.id.btn_timer);
         record_btn = findViewById(R.id.record_btn); // 버튼 참조
-        shop_btn = findViewById(R.id.shop_btn);
+        btn_shop = findViewById(R.id.btn_shop);
 
-        plant_btn.setOnClickListener(new View.OnClickListener() {
+        Animation animButtonEffect=AnimationUtils.loadAnimation(this, R.anim.anim_btn_effect);
+
+        btn_timer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn_timer.startAnimation(animButtonEffect);
                 Intent intent = new Intent(MainActivity.this , SelectHour.class);
                 startActivity(intent);
             }
@@ -39,11 +43,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        shop_btn.setOnClickListener(new View.OnClickListener() {
+        btn_shop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn_shop.startAnimation(animButtonEffect);
                 Intent intent = new Intent(MainActivity.this , ShopActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_user.startAnimation(animButtonEffect);
             }
         });
     }

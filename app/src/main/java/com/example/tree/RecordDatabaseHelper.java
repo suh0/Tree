5ecdf5b16da56bb26186178e61a08ac84ad0c2f4
suh0 <suho__kim@naver.com>
2023-12-02 05:36:03 +0900

    DB 나무와 브금 분할

--- app/src/main/java/com/example/tree/RecordDatabaseHelper.java
+++ app/src/main/java/com/example/tree/RecordDatabaseHelper.java
@@ -17,53 +17,70 @@ public class RecordDatabaseHelper extends SQLiteOpenHelper {
 
             "CREATE TABLE records (_id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, duration INTEGER, random INTEGER DEFAULT 0, hourNum INTEGER, treeIndex INTEGER);";
 
-    private static final String TABLE_PRODUCTS_CREATE =
-            "CREATE TABLE products (_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT, purchased INTEGER DEFAULT 0, price REAL DEFAULT 0.0);";
+    private static final String TABLE_TREE_PRODUCTS_CREATE =
+            "CREATE TABLE tree_products (_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT, purchased INTEGER DEFAULT 0, price REAL DEFAULT 0.0);";
+
+    private static final String TABLE_BGM_PRODUCTS_CREATE =
+            "CREATE TABLE bgm_products (_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT, purchased INTEGER DEFAULT 0, price REAL DEFAULT 0.0);";
 
     public RecordDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         SQLiteDatabase db = this.getWritableDatabase();
-        addInitialProducts(db);
+        addInitialTreeProducts(db);
+        addInitialBgmProducts(db);
         db.close();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TABLE_CREATE);
-        db.execSQL(TABLE_PRODUCTS_CREATE);
+        db.execSQL(TABLE_TREE_PRODUCTS_CREATE);
+        db.execSQL(TABLE_BGM_PRODUCTS_CREATE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS records");
-        db.execSQL("DROP TABLE IF EXISTS products");
+        db.execSQL("DROP TABLE IF EXISTS tree_products");
+        db.execSQL("DROP TABLE IF EXISTS bgm_products");
         onCreate(db);
     }
 
-    private void addInitialProducts(SQLiteDatabase db) {    //1이 구매. 기본품종은 1로 해놈
-        addProduct(db, "tree1_30m", 1, 0);
-        addProduct(db, "tree2_30m", 1, 0);
-        addProduct(db, "tree3_30m", 1, 0);
-        addProduct(db, "tree1_1h", 1, 0);
-        addProduct(db, "tree2_1h", 1, 0);
-        addProduct(db, "tree3_1h", 1, 0);
-        addProduct(db, "tree1_2h", 1, 0);
-        addProduct(db, "tree2_2h", 1, 0);
-        addProduct(db, "tree3_2h", 1, 0);
-        addProduct(db, "tree4_30m", 0, 300);
-        addProduct(db, "tree4_1h", 0, 500);
-        addProduct(db, "tree4_2h", 0, 700);
+    private void addInitialTreeProducts(SQLiteDatabase db) {    //1이 구매. 기본품종은 1로 해놈
+        addTreeProduct(db, "tree1_30m", 1, 0);
+        addTreeProduct(db, "tree2_30m", 1, 0);
+        addTreeProduct(db, "tree3_30m", 1, 0);
+        addTreeProduct(db, "tree1_1h", 1, 0);
+        addTreeProduct(db, "tree2_1h", 1, 0);
+        addTreeProduct(db, "tree3_1h", 1, 0);
+        addTreeProduct(db, "tree1_2h", 1, 0);
+        addTreeProduct(db, "tree2_2h", 1, 0);
+        addTreeProduct(db, "tree3_2h", 1, 0);
+        addTreeProduct(db, "tree4_30m", 0, 300);
+        addTreeProduct(db, "tree4_1h", 0, 500);
+        addTreeProduct(db, "tree4_2h", 0, 700);
 
-        addProduct(db, "music1", 0, 500);
-        addProduct(db, "music2", 0, 500);
-        addProduct(db, "music3", 0, 500);
     }
 
-    private void addProduct(SQLiteDatabase db, String productName, int purchased, double price) {
+    private void addInitialBgmProducts(SQLiteDatabase db){
+        addBgmProduct(db, "music1", 0, 500);
+        addBgmProduct(db, "music2", 0, 500);
+        addBgmProduct(db, "music3", 0, 500);
+    }
+
+    private void addTreeProduct(SQLiteDatabase db, String productName, int purchased, double price) {
         ContentValues values = new ContentValues();
         values.put("product_name", productName);
         values.put("purchased", purchased);
         values.put("price", price);
-        db.insert("products", null, values);
+        db.insert("tree_products", null, values);
+    }
+    private void addBgmProduct(SQLiteDatabase db, String productName, int purchased, double price) {
+        ContentValues values = new ContentValues();
+        values.put("product_name", productName);
+        values.put("purchased", purchased);
+        values.put("price", price);
+        db.insert("bgm_products", null, values);
     }
 }

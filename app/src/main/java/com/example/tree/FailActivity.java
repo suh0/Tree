8d34a59aff1e94e3c7225860d8094824028dee7d
eSeohyeon <leeseohyeon1203@gmail.com>
2023-12-03 21:42:58 +0900

    성공/실패 화면 홈 버튼 수정

--- app/src/main/java/com/example/tree/FailActivity.java
+++ app/src/main/java/com/example/tree/FailActivity.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FailActivity extends AppCompatActivity {
 
     TextView txt_fail, txt_tryAgain;
-    Button btn_home;
+    ImageView btn_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

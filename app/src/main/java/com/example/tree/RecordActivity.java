337b28249a2abdc60306be372d9d288b0a3a5778
suh0 <suho__kim@naver.com>
2023-12-05 17:33:50 +0900

    총합 ms

--- app/src/main/java/com/example/tree/RecordActivity.java
+++ app/src/main/java/com/example/tree/RecordActivity.java
@@ -27,7 +27,6 @@ public class RecordActivity extends AppCompatActivity {
 
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-
         Cursor cursor = db.rawQuery("SELECT * FROM records WHERE date LIKE ?", new String[]{"%"});
 
         int durationIndex = cursor.getColumnIndex("duration");
@@ -53,15 +52,33 @@ public class RecordActivity extends AppCompatActivity {
         cursor.close();
 
         TextView summaryTextView = new TextView(this);
-        summaryTextView.setText( "5초: " + durationsCount[0] + "번, 30분: "
+        summaryTextView.setText("5초: " + durationsCount[0] + "번, 30분: "
                 + durationsCount[1] + "번, 1시간: " + durationsCount[2] + "번, 2시간: "
                 + durationsCount[3] + "번");
         recordLayout.addView(summaryTextView);
+
+        long totalDuration = calculateTotalDuration(db);
+
+        TextView totalDurationTextView = new TextView(this);
+        totalDurationTextView.setText("총 합계 시간: " + totalDuration + "밀리초");
+        recordLayout.addView(totalDurationTextView);
 
         Button deleteButton = new Button(this);
         deleteButton.setText("데이터 삭제");
         deleteButton.setOnClickListener(v -> deleteAllData());
         recordLayout.addView(deleteButton);
+    }
+
+    private long calculateTotalDuration(SQLiteDatabase db) {
+        long totalDuration = 0;
+
+        Cursor cursor = db.rawQuery("SELECT SUM(duration) FROM records", null);
+        if (cursor.moveToFirst()) {
+            totalDuration = cursor.getLong(0);
+        }
+
+        cursor.close();
+        return totalDuration;
     }
 
     private void deleteAllData() {
